73f6a1557eb6a6f92602cb4819432eb7edad8459
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-10-22 21:16:22 +0530

    Calling/ rendering ParentForm in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
 import React from "react";
 import './../styles/App.css';
+import ParentForm from "./ParentForm";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <ParentForm/>
     </div>
   )
 }
